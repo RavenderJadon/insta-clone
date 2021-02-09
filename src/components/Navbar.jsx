80aa751a02f3a2a55e498258d1a2261216ad3597
Ravender Jadon <ravender@jnexsoft.com>
2021-02-09 13:18:05 +0530

    completed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,16 @@
+import {
+  FaSistrix,
+  FaHome,
+  FaTelegramPlane,
+  FaRegCompass,
+  FaRegHeart,
+} from "react-icons/fa";
+
 const Navbar = () => {
+  const openForm = () => {
+    console.log("hi");
+  };
+
   return (
     <div className="navbar">
       <div className="navbar__first">
@@ -8,10 +20,25 @@ const Navbar = () => {
       </div>
       <div className="navbar__middle">
         <div className="navbar__middle-search">
-          
+          <input type="text" className="navbar__search" placeholder="Search" />
+          <FaSistrix className="searchIcon" />
         </div>
       </div>
-      <div className="navbar__last"></div>
+      <div className="navbar__last">
+        <li>
+          <FaHome className="navbar__icons" />
+        </li>
+        <li>
+          <FaTelegramPlane className="navbar__icons" />
+        </li>
+        <li>
+          <FaRegCompass className="navbar__icons" />
+        </li>
+        <li>
+          <FaRegHeart className="navbar__icons" />
+        </li>
+        <li onClick={openForm}>Register/Login</li>
+      </div>
     </div>
   );
 };
