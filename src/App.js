c311aae59951e71083f3d55e7c997a266ae3ff46
Ravender Jadon <ravender@jnexsoft.com>
2021-02-12 12:29:55 +0530

    chor

--- src/App.js
+++ src/App.js
@@ -4,28 +4,10 @@ import Navbar from "./components/Navbar";
 import Modal from "./components/Model";
 
 function App() {
-
   return (
     <Context>
       <Navbar />
       <Modal />
-      <div>
-        <span>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-          <div>new</div>
-        </span>
-      </div>
     </Context>
   );
 }
