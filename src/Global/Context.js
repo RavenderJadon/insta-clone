0d758add71158f145ee61470be4f7966de34fff3
Ravender Jadon <ravender@jnexsoft.com>
2021-02-12 20:37:49 +0530

    full ui of posts

--- src/Global/Context.js
+++ src/Global/Context.js
@@ -7,6 +7,7 @@ const Context = (props) => {
   const [model, setModel] = useState(false);
   const [usr, setUser] = useState(null);
   const [loader, setLoader] = useState(true);
+  const [posts, setPosts] = useState([]);
 
   const openModal = () => {
     setModel(true);
@@ -51,28 +52,32 @@ const Context = (props) => {
   const create = (data) => {
     console.log("post data", data);
     const { title, image } = data;
-    const upload = storage.ref(`images/${image.name }`).put(image)
-          upload.on("state_changed", (snapshot)=>{
-            let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+    const upload = storage.ref(`images/${image.name}`).put(image);
+    upload.on(
+      "state_changed",
+      (snapshot) => {
+        let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
         console.log(progress);
-          },(err)=>{
-            console.log("err",err)
-          },()=>{
-            //success function/completed function
-            storage.ref("images")
-            .child(image.name)
-            .getDownloadURL()
-            .then((url)=>{
-              db.collection("posts").add({
-                title:title,
-                image:url,
-                username: usr.displayName,
-                currentTime: firebase.firestore.FieldValue.serverTimestamp(),
-              })
-
-            })
-          })
-
+      },
+      (err) => {
+        console.log("err", err);
+      },
+      () => {
+        //success function/completed function
+        storage
+          .ref("images")
+          .child(image.name)
+          .getDownloadURL()
+          .then((url) => {
+            db.collection("posts").add({
+              title: title,
+              image: url,
+              username: usr.displayName,
+              currentTime: firebase.firestore.FieldValue.serverTimestamp(),
+            });
+          });
+      }
+    );
   };
 
   useEffect(() => {
@@ -80,6 +85,23 @@ const Context = (props) => {
       setUser(user);
       setLoader(false);
     });
+
+    // fetch posts from database;
+
+    db.collection("posts")
+      .orderBy("currentTime", "desc")
+      .onSnapshot((snp) => {
+        console.log("my data", snp.docs);
+        setPosts(snp.docs.map((doc)=>(
+          {
+            id: doc.id,
+            title: doc.data().title,
+            image: doc.data().image,
+            username: doc.data().username,
+          }
+
+        )))
+      });
   }, []);
   console.log("login User :", usr);
 
@@ -95,6 +117,7 @@ const Context = (props) => {
         loader,
         logout,
         create,
+        posts, 
       }}
     >
       {props.children}
