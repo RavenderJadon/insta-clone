67267416829dd7397a58bb58bf654a3d17c610c8
Ravender Jadon <ravender@jnexsoft.com>
2021-02-12 20:16:28 +0530

    post component

--- src/App.js
+++ src/App.js
@@ -4,14 +4,16 @@ import Navbar from "./components/Navbar";
 import Modal from "./components/Model";
 import Stories from "./components/Stories";
 import Create from "./components/Create";
+import Posts from "./components/Posts";
 
 function App() {
   return (
     <Context>
       <Navbar />
       <div className="container">
-      <Stories />
-      <Create />
+        <Stories />
+        <Create />
+        <Posts />
       </div>
       <Modal />
     </Context>
---
+++ src/components/Posts.jsx
@@ -0,0 +1,15 @@
+const Posts = () => {
+  return (
+    <div className="posts">
+      <div className="posts__header">
+        <div className="posts__header-avator">s</div>
+        <div className="posts__header-name">user1</div>
+      </div>
+      <div className="posts__img">
+        <img src="" alt="post" />
+      </div>
+    </div>
+  );
+};
+
+export default Posts;
